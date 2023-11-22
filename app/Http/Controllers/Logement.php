bf565b5d4560bed772810acc24ab8b589e44cd07
Kyrill <kyrilldumerchat@gmail.com>
2023-11-22 16:54:08 +0100

    test insertion logement en bdd

--- app/Http/Controllers/Logement.php
+++ app/Http/Controllers/Logement.php
@@ -78,6 +78,6 @@ class Logement extends Controller
     }
 
     public function getInfoLogement(Request $request) {
-        dd(DB::select('select * from logement where id_logement = ?', $request->id));
+        dd(DB::select('select * from logement where id_logement = ?', [$request->id]));
     } 
 }

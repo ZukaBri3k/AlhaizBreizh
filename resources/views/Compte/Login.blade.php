25d3b20b0f991b5b0289d204c939e9943fb4dd11
ErwanFi <skyzalto@gmail.com>
2023-11-24 02:39:57 -0500

    Correction problèmes sur boutons radio (pseudo-boutons peuvent êtres côchés maintenant)

--- resources/views/Compte/Login.blade.php
+++ resources/views/Compte/Login.blade.php
@@ -33,7 +33,7 @@
                     <div id="radio_btn_placement_proprio">
 
                         <input type="radio" id="radiobtn" name="radiobtn" value="proprietaire" class="radios" />
-                        <label for="radiobtn">Propriétaire</label>    
+                        <label for="radiobtn" id="label_radio">Propriétaire</label>    
 
                     </div>
            

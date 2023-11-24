8dfae3ccfce4363aed540eea5777c27c176e907e
Kyrill <kyrilldumerchat@gmail.com>
2023-11-24 10:45:22 +0100

    test incription d'un devis en base

--- app/Http/Controllers/Devis.php
+++ app/Http/Controllers/Devis.php
@@ -28,13 +28,47 @@ class Devis extends Controller
 
     public function creerDevisDB (Request $request) {
         $tab = [
-
+            $request->nb_pers,
+            $request->date_deb,
+            $request->date_fin,
+            $request->date_em,
+            $request->date_val,
+            false,
+            $request->charges_HT,
+            $request->sous_tot_HT,
+            $request->sous_tot_TTC,
+            $request->frais_serv_HT,
+            $request->frais_serv_TTC,
+            $request->taxe_de_sejour,
+            $request->prix_tot,
+            3,
+            false,
+            $request->heure_arriv,
+            $request->heure_depart
         ];
 
         DB::insert('insert into devis (
             nb_pers,
             date_deb,
-            date_fin
-            ) values (?, ?)', [1, 'Dayle']);
+            date_fin,
+            date_em,
+            date_val,
+            annul,
+            charges_ht,
+            sous_tot_ht,
+            sous_tot_ttc,
+            frais_serv_ht,
+            frais_serv_ttc,
+            taxe_de_sejour,
+            prix_tot,
+            delai,
+            etat_devis,
+            heure_arriv,
+            heure_depart,
+            id_client_devis
+            ) values (
+            ?, ?, ?, ?, ?, ?, ?, ?, ?,
+            ?, ?, ?, ?, ?, ?, ?, ?, ?,
+            )', $tab);
     }
 }

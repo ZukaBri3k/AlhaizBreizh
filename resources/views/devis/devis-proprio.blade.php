fc93b539c693292fc64f50659cf08efc0cd8b0dc
JACOVETTI ROMAIN <rjacovetti@mx21>
2023-11-24 10:31:47 +0100

    navbar

--- resources/views/devis/devis-proprio.blade.php
+++ resources/views/devis/devis-proprio.blade.php
@@ -7,10 +7,10 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container">
     <header>
         <x-Navbar></x-Navbar>
     </header>
+    <div class="container">
     <main>
         <section class="boutons">
             <form action="index.php" method="get" target="_blank">

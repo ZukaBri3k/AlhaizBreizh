c7fa8a58f3f3359aea734ac88433d5971d3b262b
EwanP56 <ewanpenven@gmail.com>
2023-11-27 14:49:23 +0100

    Debut de l'affichage grâce au back

--- resources/views/logement/details_logement.blade.php
+++ resources/views/logement/details_logement.blade.php
@@ -125,12 +125,12 @@
         <p>Remboursement intégral jusqu’à 3 jours avant la date d’arrivée</p>
         <h1>Nombre de chambre, lit et salle de bain :</h1>
         <p>{{ $logement->nb_chambre_logement }} Chambre(s)</p>
-        @for ($i = 0; $i < $logement->nb_chambre_logement; $i++)
+        @foreach ($chambre as $values)
           <p>Chambre n°$i</p>
-          <p>Elle possède : {{ $chambre->nb_lit_simple[$i] }} lit(s) simple(s)</p>
-          <p>Elle possède : {{ $chambre->nb_lit_double[$i] }} lit(s) double(s)</p>
-          <p>Détail des lits de la chambre : {{ $chambre->details_lit[$i] }}</p>
-        @endfor
+          <p>Elle possède : {{ $values->nb_lit_simple }} lit(s) simple(s)</p>
+          <p>Elle possède : {{ $values->nb_lit_double }} lit(s) double(s)</p>
+          <p>Détail des lits de la chambre : {{ $values->details_lit }}</p>
+        @endforeach
         <hr>
         <h1>Logements similaires :</h1>
         <p>Carte de 2 logements</p>

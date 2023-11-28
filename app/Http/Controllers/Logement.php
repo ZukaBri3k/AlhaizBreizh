73821d48f96bed704e6475a0ead71d019f72a626
EwanP56 <ewanpenven@gmail.com>
2023-11-28 16:26:12 +0100

    Debut de l'affichage grâce au back

--- app/Http/Controllers/Logement.php
+++ app/Http/Controllers/Logement.php
@@ -81,7 +81,7 @@ class Logement extends Controller
         $id_proprio = DB::select('select id_proprio_logement from logement where id_logement = ?', [intval($request->id)]);
         return View("logement/details_logement" , ['logement' => DB::select('select * from logement where id_logement = ?', [intval($request->id)]) [0],  
         'chambre' => DB::select('select * from chambre where id_logement = ?', [intval($request->id)]), 
-        'nom_proprio' => DB::select('select nom_pers from personnes where id = ?', [intval($id_proprio)]), 
-        'paypal' => DB::select('select paypal_proprio from proprietaire where id_proprio = ?', [intval($id_proprio)])]);
+        'nom_proprio' => DB::select('select nom_pers from personnes where id = ?', [intval($id_proprio[0]->id_proprio_logement)]), 
+        'paypal' => DB::select('select paypal_proprio from proprietaire where id_proprio = ?', [intval($id_proprio[0]->id_proprio_logement)])]);
     } 
 }

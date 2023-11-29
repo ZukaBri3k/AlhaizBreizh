824d82d3b495b781b5752f254372a1751fe19376
Kyrill <kyrilldumerchat@gmail.com>
2023-11-29 14:38:36 +0100

    ajout de la page de creation de logement

--- resources/views/logement/creer-logement-p3.blade.php
+++ resources/views/logement/creer-logement-p3.blade.php
@@ -34,22 +34,21 @@
                     @verbatim
                             <div>
                                 <div class="abc">
-                                    <h3>Chambre {{ $i }}</h3>
+                                    <h3>Chambre @{{ $i }}</h3>
                                     <section class="p1-3-1">
                                         <p>Nombre de lit simple : </p>
                                         <p>Nombre de lit double : </p>
                                         <p>Détails des lits disponibles : </p>    
                                     </section>
                                     <section class="p1-3-2">    
-                                        <input name="nb_lit_s_{{$i}}" value = "" type="number">
-                                        <input name="nb_lit_d_{{$i}}" value = "" type="number">
-                                        <textarea name="detail_lits_{{$i}}" placeholder="Saisissez içi (255 caractères max)" value = "" type="text">
+                                        <input name="nb_lit_s_@{{$i}}" value = "" type="number">
+                                        <input name="nb_lit_d_@{{$i}}" value = "" type="number">
+                                        <textarea name="detail_lits_@{{$i}}" placeholder="Saisissez içi (255 caractères max)" value = "" type="text">
                                     </section>    
                                 </div>
                             </div>
                     @endverbatim
                 @endfor
-            @endphp
         </section>
         <section class="p2">
         </section>

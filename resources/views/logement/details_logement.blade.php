12bbc46e6236712e6eacbfab1b2c208958d752ac
EwanP56 <ewanpenven@gmail.com>
2023-11-30 00:10:25 +0100

    correction page cevric

--- resources/views/logement/details_logement.blade.php
+++ resources/views/logement/details_logement.blade.php
@@ -280,64 +280,6 @@
         @endif
         <br>
 
-        
-        <script src="https://polyfill.io/v3/polyfill.min.js?features=default"></script>
-
-        <link rel="stylesheet" type="text/css" href="./style.css" />
-        <script type="module" src="./index.js"></script>
-      </head>
-      <body>
-        <h3>My Google Maps Demo</h3>
-        <!--The div element for the map -->
-        <div id="map"></div>
-
-        <!-- prettier-ignore -->
-        <script>(g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})
-            ({key: "AIzaSyB41DRUbKWJHPxaFjMAwdrzWzbVKartNGg", v: "beta"});</script>
-
-        <script> let map;
-          async function initMap() {
-            // The location of Uluru
-            const position = { lat: {{ $logement->latitude_logement }}, lng: {{ $logement->longitude_logement }} };
-            // Request needed libraries.
-            //@ts-ignore
-            const { Map } = await google.maps.importLibrary("maps");
-            const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
-
-            // The map, centered at Uluru
-            map = new Map(document.getElementById("map"), {
-              zoom: 4,
-              center: position,
-              mapId: "DEMO_MAP_ID",
-            });
-
-            // The marker, positioned at Uluru
-            const marker = new AdvancedMarkerView({
-              map: map,
-              position: position,
-              title: "Uluru",
-            });
-          }
-
-          initMap(); </script>
-        </div>
-        <style>
-            #map {
-  height: 100%;
-}
-
-/*
- * Optional: Makes the sample page fill the window.
- */
-html,
-body {
-  height: 100%;
-  margin: 0;
-  padding: 0;
-}
-        </style>
-
-
       <div class="leStick">
           <p>à partir de : {{ $logement->prix_logement }} / mois</p>
           <p>Propriétaire : {{ $nom_proprio[0]->nom_pers }}</p>

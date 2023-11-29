f27b1d6e4ab74dcf8ffb67f90bb9c552ed046af5
EwanP56 <ewanpenven@gmail.com>
2023-11-29 19:11:13 +0100

    css details previsu

--- resources/views/logement/details_logement_previsu.blade.php
+++ resources/views/logement/details_logement_previsu.blade.php
@@ -310,7 +310,7 @@
             <button type="button">Retour</button>
         </a>
         <a href="{{route('devis-client')}}">
-            <button type="button">Enregistrer</button>
+            <button type="button" id="enreg_buton">Enregistrer</button>
         </a>
       </div>  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>

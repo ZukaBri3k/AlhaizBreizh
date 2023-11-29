a0c6d7da06c419cf39f5e439f6636498892cbddb
RSymbiose <118276398+RSymbiose@users.noreply.github.com>
2023-11-29 19:17:30 +0100

    Update Devis.php

--- app/Http/Controllers/Devis.php
+++ app/Http/Controllers/Devis.php
@@ -49,26 +49,9 @@ class Devis extends Controller
             INSERT INTO devis (
             nb_pers,
             date_deb,
-            date_fin,
-            date_em,
-            date_val,
-            annul,
-            charges_ht,
-            sous_tot_ht,
-            sous_tot_ttc,
-            frais_serv_ht,
-            frais_serv_ttc,
-            taxe_de_sejour,
-            prix_tot,
-            delai,
-            etat_devis,
-            heure_arriv,
-            heure_depart,
-            id_client_devis,
-            id_proprio
+            date_fin
             ) values (
-            ?, ?, ?, ?, ?, ?, ?, ?, ?,
-            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+            ?, ?, ?
             )', [$nbPers, $dateDeb, $dateFin]);
         DB::insert('INSERT INTO devis (id_client_devis) values (4)');
         

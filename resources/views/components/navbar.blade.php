c3a50791cf333840203e1321ab9d2b333307ed0d
RSymbiose <118276398+RSymbiose@users.noreply.github.com>
2023-12-18 10:14:04 +0100

    Update navbar.blade.php

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,4 @@
-<!-- <link rel="stylesheet" href="{{asset('css/main.css')}}">-->
-<link rel="stylesheet" href="{{asset('css/connexion.css')}}">
+<link rel="stylesheet" href="{{asset('css/main.css')}}">
 @auth
 @if ($role == 1)
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">

73b33fda4809ad37de0f5281ff5e0281f1b9be9e
ZukaBri3k <kyrilldumerchat@gmail.com>
2023-12-26 16:05:10 +0100

    ajout page d'accueil

---
+++ app/View/Components/Card.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\View\Component;
+use Illuminate\Contracts\View\View;
+
+class Card extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public string $title,
+        public string $desc,
+        public string $photo,
+        public int $note
+        )
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.card');
+    }
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,242 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <title>Laravel</title>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link rel="stylesheet" href="./style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <x-Navbar></x-Navbar>
+    <div>
+        <div class="video-container">
+            <video src="./video_accueil.mp4" autoplay type="video/mp4" muted loop preload="auto"></video>
+        </div>
+        <h1>Découvrez les meilleurs<br> logements<br> de toute la Bretagne</h1>
+    </div>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    </head>
-    <body>
-        <x-Navbar></x-Navbar>
-        <a href="{{ route('details', ['id' => 4]) }}">Villa stylé</a>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-    </body>
+    <section class="mieuxNote">
+        <h2>Logements les mieux notés :</h2>
+        <div class="liste-card">
+
+            <a class="lienCard" href="google.com">
+    
+                <div class="card">
+                    <img src="maison.jpg" alt="maison">
+        
+                    <div>
+                        <div class="premLigne">
+                            <h3>Titre ceci est un titre un titre</h3>
+                            <div class="note">
+                                <i class="icon"></i>
+                                <p>5.0</p>
+                            </div>            
+                        </div>
+                        <div class="desc">
+                            <p>Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                            </p>
+                        </div>
+                    </div>
+                    <div class="prix">
+                        <p><strong>100€</strong> par nuit</p>
+                    </div>
+                </div> 
+            </a>
+
+            <a class="lienCard" href="google.com">
+    
+                <div class="card">
+                    <img src="maison.jpg" alt="maison">
+        
+                    <div>
+                        <div class="premLigne">
+                            <h3>Titre ceci est un titre un titre</h3>
+                            <div class="note">
+                                <i class="icon"></i>
+                                <p>5.0</p>
+                            </div>            
+                        </div>
+                        <div class="desc">
+                            <p>Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                            </p>
+                        </div>
+                    </div>
+                    <div class="prix">
+                        <p><strong>100€</strong> par nuit</p>
+                    </div>
+                </div> 
+            </a>
+
+            <a class="lienCard" href="google.com">
+    
+                <div class="card">
+                    <img src="maison.jpg" alt="maison">
+        
+                    <div>
+                        <div class="premLigne">
+                            <h3>Titre ceci est un titre un titre</h3>
+                            <div class="note">
+                                <i class="icon"></i>
+                                <p>5.0</p>
+                            </div>            
+                        </div>
+                        <div class="desc">
+                            <p>Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                            </p>
+                        </div>
+                    </div>
+                    <div class="prix">
+                        <p><strong>100€</strong> par nuit</p>
+                    </div>
+                </div> 
+            </a>
+        </div>
+    </section>
+
+    <section class="autres">
+        <h2>Nos autres logements</h2>
+        <div class="liste-logement">
+            <a class="lienCard" href="google.com">
+    
+                <div class="card">
+                    <img src="maison.jpg" alt="maison">
+        
+                    <div>
+                        <div class="premLigne">
+                            <h3>Titre ceci est un titre un titre</h3>
+                            <div class="note">
+                                <i class="icon"></i>
+                                <p>5.0</p>
+                            </div>            
+                        </div>
+                        <div class="desc">
+                            <p>Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                            </p>
+                        </div>
+                    </div>
+                    <div class="prix">
+                        <p><strong>100€</strong> par nuit</p>
+                    </div>
+                </div> 
+            </a>
+
+            <a class="lienCard" href="google.com">
+    
+                <div class="card">
+                    <img src="maison.jpg" alt="maison">
+        
+                    <div>
+                        <div class="premLigne">
+                            <h3>Titre ceci est un titre un titre</h3>
+                            <div class="note">
+                                <i class="icon"></i>
+                                <p>5.0</p>
+                            </div>            
+                        </div>
+                        <div class="desc">
+                            <p>Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                            </p>
+                        </div>
+                    </div>
+                    <div class="prix">
+                        <p><strong>100€</strong> par nuit</p>
+                    </div>
+                </div> 
+            </a>
+
+            <a class="lienCard" href="google.com">
+    
+                <div class="card">
+                    <img src="maison.jpg" alt="maison">
+        
+                    <div>
+                        <div class="premLigne">
+                            <h3>Titre ceci est un titre un titre</h3>
+                            <div class="note">
+                                <i class="icon"></i>
+                                <p>5.0</p>
+                            </div>            
+                        </div>
+                        <div class="desc">
+                            <p>Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                            </p>
+                        </div>
+                    </div>
+                    <div class="prix">
+                        <p><strong>100€</strong> par nuit</p>
+                    </div>
+                </div> 
+            </a>
+
+            <a class="lienCard" href="google.com">
+    
+                <div class="card">
+                    <img src="maison.jpg" alt="maison">
+        
+                    <div>
+                        <div class="premLigne">
+                            <h3>Titre ceci est un titre un titre</h3>
+                            <div class="note">
+                                <i class="icon"></i>
+                                <p>5.0</p>
+                            </div>            
+                        </div>
+                        <div class="desc">
+                            <p>Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                            </p>
+                        </div>
+                    </div>
+                    <div class="prix">
+                        <p><strong>100€</strong> par nuit</p>
+                    </div>
+                </div> 
+            </a>
+
+            <a class="lienCard" href="google.com">
+    
+                <div class="card">
+                    <img src="maison.jpg" alt="maison">
+        
+                    <div>
+                        <div class="premLigne">
+                            <h3>Titre ceci est un titre un titre</h3>
+                            <div class="note">
+                                <i class="icon"></i>
+                                <p>5.0</p>
+                            </div>            
+                        </div>
+                        <div class="desc">
+                            <p>Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                                Ceci est une courte description d'un logement afin de le décrire... voilà voilà
+                            </p>
+                        </div>
+                    </div>
+                    <div class="prix">
+                        <p><strong>100€</strong> par nuit</p>
+                    </div>
+                </div> 
+            </a>
+        </div>
+    </section>
+
+    <x-Footer></x-Footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
 </html>

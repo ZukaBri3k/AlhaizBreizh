89cfbcda5272618c8d2fe770d6c34f9b99109254
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-01-22 17:16:56 +0100

    modif page accueil

--- app/Http/Controllers/Welcome.php
+++ app/Http/Controllers/Welcome.php
@@ -10,13 +10,21 @@ class Welcome extends Controller
 {
     public function affichage() {
 
-        $logements = DB::select("SELECT * FROM logement");
+        $logements = DB::select("SELECT * FROM logement LIMIT 4");
 
         foreach ($logements as $logement) {
             $logement->lien = "/logement/" . $logement->id_logement . "/details";
             $logement->id = $logement->id_logement;
         }
 
-        return view('welcome', ['logements' => $logements]);
+
+        $logementsRecents = DB::select("SELECT * FROM logement ORDER BY id_logement DESC");
+
+        foreach ($logementsRecents as $logement) {
+            $logement->lien = "/logement/" . $logement->id_logement . "/details";
+            $logement->id = $logement->id_logement;
+        }
+
+        return view('welcome', ['logements' => $logements, 'logementsRecents' => $logementsRecents]);
     }
 }

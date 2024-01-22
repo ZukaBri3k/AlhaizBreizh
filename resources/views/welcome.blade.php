dcde9644e227a8f3fa1186249a2076cf66c8c22f
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-01-22 18:08:58 +0100

    modif page accueil

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -29,7 +29,7 @@
         <h2>Nos logements les plus récents</h2>
         <div class="liste-card">
             @foreach ($logementsRecents as $logement)
-                <x-Card titre="{{$logement->libelle_logement}}" desc="{{$logement->accroche_logement}}" note=0 prix="{{$logement->prix_logement}}" lien="{{$logement->lien}}" id="{{$logement->id}}"></x-Card>
+                <x-Card titre="{{$logement->libelle_logement}}" desc="{{$logement->accroche_logement}}" note="{{$logement->moyenne_avis_logement}}" prix="{{$logement->prix_logement}}" lien="{{$logement->lien}}" id="{{$logement->id}}"></x-Card>
             @endforeach
         </div>
     </section>

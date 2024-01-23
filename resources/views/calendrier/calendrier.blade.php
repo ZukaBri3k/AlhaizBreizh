b449f100e3e4ed093953a345c2ed90e755604b24
jules.denef <jules.denef@etudiant.univ-rennes1.fr>
2024-01-23 14:03:55 +0100

    fin de  separation

--- resources/views/calendrier/calendrier.blade.php
+++ resources/views/calendrier/calendrier.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js'></script>  
-  
+  <link rel="stylesheet" type="text/css" href="{{asset('css/cal.css')}}">
 </head>
 <body>
 
@@ -28,71 +28,7 @@
 </div>
 
 <!-- Cloudflare Pages Analytics --><script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "dc4641f860664c6e824b093274f50291"}'></script><!-- Cloudflare Pages Analytics -->  
-<style>
-  html, body {
-  margin: 0;
-  padding: 0;
-  font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-  font-size: 14px;
-}
 
-#external-events {
-  position: fixed;
-  z-index: 2;
-  top: 20px;
-  left: 20px;
-  width: 150px;
-  padding: 0 10px;
-  border: 1px solid #ccc;
-  background: beige;
-}
-
-#external-events .fc-event {
-  cursor: move;
-  margin: 3px 0;
-}
-
-#calendar-container {
-  position: relative;
-  z-index: 1;
-  margin-left: 200px;
-}
-
-#calendar {
-  max-width: 1100px;
-  margin: 20px auto;
-}
-
-.fc-h-event .fc-event-main  {
-  color: black; 
-  font-size: large;
-  font-style: italic;
-}
-
-  /* Style pour un événement spécifique (par exemple, réservé) */
-  .fc-event-draggable.fc-event[data-title="réservé"] {
-    background-color: red !important;
-    color: white !important;
-  }
-
-  /* Style pour un autre événement spécifique (par exemple, indisponible) */
-  .fc-event-draggable.fc-event[data-title="indisponible"] {
-    background-color: gray !important;
-    color: white !important;
-  }
-  .fc-h-eventindis{
-  background-color: gray !important;
-  color: black; 
-  font-size: large;
-  font-style: italic;
- }
- .fc-h-eventres{
-  background-color: red !important;
-  color: black; 
-  font-size: large;
-  font-style: italic;
- }
-  </style>
 
 
 <script src="{{asset('js/script_cal.js')}}"></script>

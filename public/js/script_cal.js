4cca5d13250808b351fc86d677f06b3366e9a592
jules.denef <jules.denef@etudiant.univ-rennes1.fr>
2024-01-23 16:58:57 +0100

    je prie2

--- public/js/script_cal.js
+++ public/js/script_cal.js
@@ -87,7 +87,7 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log('Données envoyées avec succès !', response);
       },
       error: function(error) {
-        console.error('Erreur lors de l\'envoi des données :', error);
+        alert('Erreur lors de l\'envoi des données :', error);
       }
     });
   });

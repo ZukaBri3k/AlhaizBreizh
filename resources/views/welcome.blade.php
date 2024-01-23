55c52906c14ca350e4df2059383ae06084c079e2
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-01-23 17:40:35 +0100

    debut du trie des logements

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
                     listeLogement.sort((a, b) => a.prix_logement - b.prix_logement);
                     
                     let ListeCard = document.getElementsByClassName("liste-card")[1];
-                    console.log(ListeCard.children[0].children[0].children[2].children[0].children[0].value);
+                    console.log(ListeCard.children[0].children[0].children[2].children[0].children[0].innerHTML);
                     let card = ListeCard.children[0];
                     ListeCard.removeChild(card);
                     ListeCard.appendChild(card);

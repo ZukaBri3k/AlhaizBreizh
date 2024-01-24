932e5f0994b06f6c2aabe61c3ac58ba08c48c6c7
jules.denef <jules.denef@etudiant.univ-rennes1.fr>
2024-01-25 00:12:37 +0100

    bach dans les oreilles

--- public/js/script_cal.js
+++ public/js/script_cal.js
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function() {
   
   document.getElementById('my-button').addEventListener('click', function() {
     // Vérifier s'il y a des événements actuellement présents sur le calendrier
-    var events = calendar.getDate('clientEvents');
+    var events = calendar.getDate('EventE1');
     alert(events);
         if (events.length > 0) {
       // Si un événement est présent, obtenir la date du calendrier

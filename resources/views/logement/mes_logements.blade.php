0fec6532d2677a2f6511cfb7d15274c2ddd51a46
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-01-24 23:15:47 +0100

    ajout de la partie devis sur la page mes logements

--- resources/views/logement/mes_logements.blade.php
+++ resources/views/logement/mes_logements.blade.php
@@ -54,44 +54,46 @@
             if(count($tabReserv) == 0){
                 echo "<p class='aucuneReservation'>Vous n'avez aucune réservation.</p>";
             }
-        @endphp
-        <div class="listeMesReservations">
-            <div class="btnTriFiltre">
-                <script>
-                    let tri = 0;
-                    function triDate() {
-                        let ListeDevis = document.querySelectorAll(".listeMesReservations .devis");
-                        let tabDevis = Array.from(ListeDevis);
-                        let btnTriDate = document.querySelector("#btnTriDate");
-                        
-                        if(tri == 0) {
-                            tri = 1;
-                            btnTriDate.innerHTML = "Trier par date (du plus ancien)";
-                            tabDevis.sort((a, b) => {
-                                let prixA = parseInt(a.classList[1]);
-                                let prixB = parseInt(b.classList[1]);
-                                return prixA - prixB;
-                            });
-                        } else {
-                            tri = 0;
-                            btnTriDate.innerHTML = "Trier par date (du plus récent)";
-                            tabDevis.sort((a, b) => {
-                                let prixA = parseInt(a.classList[1]);
-                                let prixB = parseInt(b.classList[1]);
-                                return prixB - prixA;
-                            });
-                        }
+            @endphp
 
-                        let conteneurDevis = document.querySelector(".listeMesReservations");
-                        conteneurDevis.innerHTML = "";
-
-                        tabDevis.forEach((carte) => {
-                            conteneurDevis.appendChild(carte);
-                        });    
+        <div class="btnTriFiltre">
+            <script>
+                let tri = 0;
+                function triDate() {
+                    let ListeDevis = document.querySelectorAll(".listeMesReservations .devis");
+                    let tabDevis = Array.from(ListeDevis);
+                    let btnTriDate = document.querySelector("#btnTriDate");
+                    
+                    if(tri == 0) {
+                        tri = 1;
+                        btnTriDate.innerHTML = "Trier par date (du plus ancien)";
+                        tabDevis.sort((a, b) => {
+                            let prixA = parseInt(a.classList[1]);
+                            let prixB = parseInt(b.classList[1]);
+                            return prixA - prixB;
+                        });
+                    } else {
+                        tri = 0;
+                        btnTriDate.innerHTML = "Trier par date (du plus récent)";
+                        tabDevis.sort((a, b) => {
+                            let prixA = parseInt(a.classList[1]);
+                            let prixB = parseInt(b.classList[1]);
+                            return prixB - prixA;
+                        });
                     }
-                </script>
-                <button id="btnTriDate" onclick="triDate()">Trier par date (du plus récent)</button>
-            </div>
+
+                    let conteneurDevis = document.querySelector(".listeMesReservations");
+                    conteneurDevis.innerHTML = "";
+
+                    tabDevis.forEach((carte) => {
+                        conteneurDevis.appendChild(carte);
+                    });    
+                }
+            </script>
+            <button id="btnTriDate" onclick="triDate()">Trier par date (du plus récent)</button>
+        </div>
+        
+        <div class="listeMesReservations">
             @foreach($tabReserv as $reserv)
                 <x-Reservation libelle="{{$reserv->libelle_logement}}" pseudo="{{$reserv->pseudo_pers}}" dated="{{$reserv->date_deb}}" datef="{{$reserv->date_fin}}" id="{{$reserv->id_logement}}" iddevis="{{$reserv->ref_devis}}" idreservation="{{$reserv->id_reserv}}" prix="{{$reserv->prix_tot}}"></x-Reservation>
             @endforeach

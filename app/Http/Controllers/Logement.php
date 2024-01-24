b63ae6b7ef57808710d0ac20abafa3e50baf0f01
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-01-24 20:49:44 +0100

    ajout de la partie devis sur la page mes logements

--- app/Http/Controllers/Logement.php
+++ app/Http/Controllers/Logement.php
@@ -102,7 +102,7 @@ class Logement extends Controller
             $logement->id = $logement->id_logement;
         }
         
-        $tabDevis = DB::select("select * from reservation inner join devis on reservation.facture_reserv = devis.ref_devis inner join personnes on personnes.id = devis.id_client_devis inner join logement on logement.id_logement = reservation.id_logement_reserv");
+        $tabDevis = DB::select("select * from reservation inner join devis on reservation.facture_reserv = devis.ref_devis inner join personnes on personnes.id = devis.id_client_devis inner join logement on logement.id_logement = reservation.id_logement_reserv where devis.id_proprio = ? and devis.etat_devis = false", [$id]);
 
         //dd($tabDevis);
 

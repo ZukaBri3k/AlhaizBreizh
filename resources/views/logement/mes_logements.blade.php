71b5693f6e0b8beb1eedb0c12453f53e14cff288
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-01-24 18:01:14 +0100

    début page mes logements

--- resources/views/logement/mes_logements.blade.php
+++ resources/views/logement/mes_logements.blade.php
@@ -40,7 +40,7 @@
         <div class="listeMesDevis">
             @foreach($tabDevis as $devis)
                 {{dd($devis)}}
-                <x-DemandeDevis libelle_logement="{{$devis->libelle_logement}}" pseudo_client="{{$devis->pseudo_pers}}" date_deb="{{$devis->date_deb}}" date_fin="{{$devis->date_fin}}" id_logement="{{$devis->id_logement}}"></x-DemandeDevis>
+                <x-Demandedevis libelle_logement="{{$devis->libelle_logement}}" pseudo_client="{{$devis->pseudo_pers}}" date_deb="{{$devis->date_deb}}" date_fin="{{$devis->date_fin}}" id_logement="{{$devis->id_logement}}"></x-DemandeDevis>
             @endforeach
         </div>
         <hr>

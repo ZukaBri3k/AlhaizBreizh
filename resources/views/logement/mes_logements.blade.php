d9dd3a5bbc8b4fd2735863a32154955a27e55bf2
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-01-24 20:13:59 +0100

    ajout de la partie devis sur la page mes logements

--- resources/views/logement/mes_logements.blade.php
+++ resources/views/logement/mes_logements.blade.php
@@ -46,7 +46,7 @@
     </section>
 
 
-
+    <x-Footer></x-Footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>

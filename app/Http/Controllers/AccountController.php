6bf2653e38b92c636cdf5299e1b5404072eb61fc
EwanP56 <ewanpenven@gmail.com>
2024-01-24 15:38:25 +0100

    Test connexion

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -85,7 +85,7 @@ class AccountController extends Controller
             $id_personnes = DB::select('select * from personnes where id = ?', [intval($request->id)]);
             return View("Compte/MonCompteProprietaire" , ['personnes' => DB::select('select * from personnes where id = ?', [intval($id_personnes[0]->id)]) [0],
                 'cles' => DB::select('select * from cle where id_personnes = ?', [intval($id_personnes[0]->id)]),
-                'proprietaire' => DB::select('select * from proprietaire where id_proprio = ?', [intval($id_personnes[0]->id)])]);
+                'proprietaire' => DB::select('select * from proprietaire where id_proprio = ?', [intval($id_personnes[0]->id)]) [0]]);
         }
     }
     //--------------------------------------------------------------

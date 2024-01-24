3d16e2710a85c944aee8856b42157245f9919463
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-01-24 21:09:54 +0100

    ajout de la partie devis sur la page mes logements

--- resources/views/components/demande-devis.blade.php
+++ resources/views/components/demande-devis.blade.php
@@ -3,9 +3,9 @@
         <img src="{{asset('/img/logements/logement' . $id . '/couverture.jpg')}}" alt="photo du logement">
         <p>{{$libelle}}</p>
     </a>
-    <p>client : {{$pseudo}}</p>
-    <p>Du {{$dated}}</p>
-    <p>au {{$datef}}</p>
+    <p><strong>Client :</strong> {{$pseudo}}</p>
+    <p><strong>Du</strong> {{$dated}}</p>
+    <p><strong>au</strong> {{$datef}}</p>
     <div class="btn">
         <a href="{{route('validerDevis', ['id_devis' => $iddevis])}}" class="validerDevis">Accepter le devis</a>
         <a href="{{route('refuserDevis', ['id_devis' => $iddevis, 'id_reserv' => $idreservation])}}" class="refuserDevis">Refuser le devis</a>

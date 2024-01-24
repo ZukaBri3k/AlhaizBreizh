47c02b10ba9d17c1b32f4f6d23899fd0a7fc36bd
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-01-24 23:19:58 +0100

    ajout de la partie devis sur la page mes logements

--- resources/views/logement/mes_logements.blade.php
+++ resources/views/logement/mes_logements.blade.php
@@ -68,16 +68,16 @@
                         tri = 1;
                         btnTriDate.innerHTML = "Trier par date (du plus ancien)";
                         tabDevis.sort((a, b) => {
-                            let prixA = parseInt(a.classList[1]);
-                            let prixB = parseInt(b.classList[1]);
+                            let prixA = Date(a.classList[1]);
+                            let prixB = Date(b.classList[1]);
                             return prixA - prixB;
                         });
                     } else {
                         tri = 0;
                         btnTriDate.innerHTML = "Trier par date (du plus récent)";
                         tabDevis.sort((a, b) => {
-                            let prixA = parseInt(a.classList[1]);
-                            let prixB = parseInt(b.classList[1]);
+                            let prixA = Date(a.classList[1]);
+                            let prixB = Date(b.classList[1]);
                             return prixB - prixA;
                         });
                     }
@@ -92,7 +92,7 @@
             </script>
             <button id="btnTriDate" onclick="triDate()">Trier par date (du plus récent)</button>
         </div>
-        
+
         <div class="listeMesReservations">
             @foreach($tabReserv as $reserv)
                 <x-Reservation libelle="{{$reserv->libelle_logement}}" pseudo="{{$reserv->pseudo_pers}}" dated="{{$reserv->date_deb}}" datef="{{$reserv->date_fin}}" id="{{$reserv->id_logement}}" iddevis="{{$reserv->ref_devis}}" idreservation="{{$reserv->id_reserv}}" prix="{{$reserv->prix_tot}}"></x-Reservation>

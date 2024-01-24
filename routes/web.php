50dd9ab76bdf7668c1871d871b6756c36d7c7b97
EwanP56 <ewanpenven@gmail.com>
2024-01-24 15:08:26 +0100

    Test connexion

--- routes/web.php
+++ routes/web.php
@@ -44,7 +44,7 @@ Route::prefix('/logement')->group(function() {
     Route::get('/{id}/details', [Logement::class, 'getInfoLogement'])->where('id', '[0-9]+')->name('details');
     Route::get('/{id}/details_previsu', [Logement::class, 'getInfoLogementPrevisu'])->where('id', '[0-9]+')->name('details_previsu')->middleware(['auth', 'isProprietaire']);
 
-    Route::get('/creation/{page}', [Logement::class, "creation"])->where('page', '[0-8]')->name('creer_logement')->middleware(['auth', 'isProprietaire']);
+    Route::post('mise_en_ligne_logement', [Logement::class, 'ajouterLogementDB'])->name('creation_logement')->middleware(['auth', 'isProprietaire']);
 });
 
 Route::prefix('/account')->group(function () {

13aa8314f31ddec11e44daa471825aa3f2afc4f5
EwanP56 <ewanpenven@gmail.com>
2024-01-25 01:49:54 +0100

    detail logement css

--- resources/views/logement/details_logement.blade.php
+++ resources/views/logement/details_logement.blade.php
@@ -355,13 +355,14 @@
           </form>
           <script>
             function showPopup() {
-                alert("Votre demande de devis a été créée avec succès.");
-                return true; // retourne true pour permettre la soumission du formulaire
+              swal("Succès", "Votre demande de devis a été créée avec succès.", "success");
+                return true;
             }
           </script>
       </div>
     </div>
     <x-FooterClient></x-FooterClient>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>

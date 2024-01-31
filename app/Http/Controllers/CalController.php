b8486e423beea7c7a3b9e8bcea8521fedfa85e89
jules.denef <jules.denef@etudiant.univ-rennes1.fr>
2024-01-31 10:00:18 +0100

    c'est repartie

--- app/Http/Controllers/CalController.php
+++ app/Http/Controllers/CalController.php
@@ -14,6 +14,7 @@ class CalController extends Controller
 {
     $date = $request->input('events');
      $formattedDate = Carbon::parse($date)->format('Y-m-d H:i:s');
+     dd($formattedDate);
    
 
         

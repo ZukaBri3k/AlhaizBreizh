2c718bc9eafbb4ed248aadc0a173fc5a53fbd6dc
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-01-31 14:38:53 +0100

    ajout des photos de logement en bdd

--- resources/views/logement/details_logement_previsu.blade.php
+++ resources/views/logement/details_logement_previsu.blade.php
@@ -18,6 +18,7 @@
           <div class="carousel-inner" id="carousel">
             <div class="carousel-item active">
               <img src="{{ asset('storage/logement' . $logement->id_logement . '/couverture.jpg') }}" class="d-block w-100">
+              @php dd($nb_photo); @endphp
               @for($i = 1; $i < intval($nb_photo); $i++)
                 <div>
                   <img src="{{asset('storage/logement' . $logement->id_logement . '/img' . $i . '.jpg')}}" class="d-block w-100">

3f9717dccc517a6f2272530311b50b9eeee907c8
EwanP56 <ewanpenven@gmail.com>
2024-01-31 17:12:31 +0100

    Pop up pour suppression de clé

--- resources/views/Compte/MonCompteClient.blade.php
+++ resources/views/Compte/MonCompteClient.blade.php
@@ -142,7 +142,7 @@
                                     <p>Clé :&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</p>
                                     <p>" . $cleShort . "</p>
                                     <button onclick='copierTexte(event, \"$cleEscaped\")' class='button_copie'>Copie</button>
-                                    <a href='$url', class='a_api'><button class='button_api'>Supprimer sa clé</button></a>
+                                    <a href='$url' id='delete-link' class='a_api'><button class='button_api'>Supprimer sa clé</button></a>
                                 </div>
                                 <hr>";
                         }
@@ -171,6 +171,25 @@
                                 });
                             })
                     }
+
+                    document.getElementById('delete-link').addEventListener('click', function(event) {
+                    event.preventDefault();
+                    var url = this.href;
+
+                    Swal.fire({
+                        title: "Are you sure?",
+                        text: "You won't be able to revert this!",
+                        icon: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "#3085d6",
+                        cancelButtonColor: "#d33",
+                        confirmButtonText: "Yes, delete it!"
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            window.location.href = url;
+                        }
+                    });
+                });
                 </script>
             </div>
             <form action="{{route('genereCle')}} " method="post" class="api">

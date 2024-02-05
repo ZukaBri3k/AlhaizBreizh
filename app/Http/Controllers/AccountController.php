49eaf3fec25052e1fc2b3dba13f3438f8bd16f4b
EwanP56 <ewanpenven@gmail.com>
2024-02-05 08:43:12 +0100

    prblm font

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -42,6 +42,12 @@ class AccountController extends Controller
     public function deleteClient() {
 
         $id = auth()->user()->id;
+        $ref_devis = DB::select('select ref_devis from personnes where id_client_devis = ?', [$id]);
+        dd($ref_devis);
+        foreach ($ref_devis as $ref) {
+            DB::delete('delete from reservation where ref_devis = ?', [$ref->ref_devis]);
+        }
+        DB::delete('delete from devis where id_client_devis = ?', [$id]);
         DB::delete('delete from client where id_client = ?', [$id]);
         DB::delete('delete from personnes where id = ?', [$id]);
 

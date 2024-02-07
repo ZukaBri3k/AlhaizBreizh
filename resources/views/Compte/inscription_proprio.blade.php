97d4bc08e8ed093e7634e95bd730f6e353926eb5
EwanP56 <ewanpenven@gmail.com>
2024-02-07 18:14:31 +0100

    test modif client

--- resources/views/Compte/inscription_proprio.blade.php
+++ resources/views/Compte/inscription_proprio.blade.php
@@ -4,6 +4,7 @@
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/country-select-js/2.1.0/css/countrySelect.min.css">
     <link rel="stylesheet" type="text/css" href="{{asset('css/main.css')}}" />
     <link rel="stylesheet" type="text/css" href="{{asset('css/inscription.css')}}">
     <meta charset="UTF-8">
@@ -12,7 +13,10 @@
 </head>
 
 <body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/country-select-js/2.1.0/js/countrySelect.min.js"></script>
     <x-Navbar></x-Navbar>
     <main class="container">
         <h1 class="text-center">Création de votre compte propriétaire</h1>
@@ -60,7 +64,13 @@
                     <input type="text" id="ville_pers" name="ville_pers" placeholder="Entrez votre ville" class="form-control" maxlength="60" required>
 
                     <label for="pays_pers">Pays:</label>
-                    <input type="text" id="pays_pers" name="pays_pers" placeholder="Entrez votre pays" class="form-control" maxlength="25" required>
+                    <input type="text" id="pays_pers" name="pays_pers" placeholder="Entrez votre pays" value="{!! $personnes->pays_pers !!}" class="form-control" required>
+                    <script>
+                        jQuery(document).ready(function() {
+                            jQuery("#pays_pers").countrySelect();
+                        });
+                    </script>
+
                     <label for="profile-pic">
                         Insérer une photo de profil :
                         <span class="upload-icon"><img src="{{asset('/img/Download.png')}}"></span>

d685138a13efcbc9fe1dd6226977949a1dd5463d
EwanP56 <ewanpenven@gmail.com>
2024-02-08 15:16:43 +0100

    test modification client pp

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -7,6 +7,7 @@ use App\Models\Personne;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\File;
 
 

f8da1e3eae14c3c6d4cd865eb8bdef12f3d116cc
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-03-29 16:23:44 +0100

    Update x-Card component in mes_logements.blade.php

--- resources/views/logement/mes_logements.blade.php
+++ resources/views/logement/mes_logements.blade.php
@@ -25,7 +25,7 @@
                     $counter++; 
                     @endphp
                         <div class="logementEnLigne">
-                            <x-Card titre="{{$logement->libelle_logement}}" desc="{{$logement->accroche_logement}}" note="{{$logement->moyenne_avis_logement}}" prix="{{$logement->prix_logement}}" lien="{{$logement->lien}}" id="{{$logement->id}}" natLogement="{{$logement->nature_logement}}"></x-Card>
+                            <x-Card titre="{{$logement->libelle_logement}}" desc="{{$logement->accroche_logement}}" note="{{$logement->moyenne_avis_logement}}" prix="{{$logement->prix_logement}}" lien="{{$logement->lien}}" id="{{$logement->id}}" natLogement="{{$logement->nature_logement}}" ville=""></x-Card>
                             <a class="btnHL {{$classBtnHL}}" href="{{route('setHL', ['id' => $logement->id])}}">{{$textbouton}}</a>
                             <a class="btnHL MODIF" href="{{route('updateLogement', ['id' => $logement->id])}}">Modifier</a>
                             <a class="btnHL SUPPR" href="{{route('delLogement', ['id' => $logement->id])}}">Supprimer</a>
@@ -54,7 +54,7 @@
                 $counterEL++;
                 @endphp
                     <div class="logementEnLigne">
-                        <x-Card titre="{{$logement->libelle_logement}}" desc="{{$logement->accroche_logement}}" note="{{$logement->moyenne_avis_logement}}" prix="{{$logement->prix_logement}}" lien="{{$logement->lien}}" id="{{$logement->id}}" natLogement="{{$logement->nature_logement}}"></x-Card>
+                        <x-Card titre="{{$logement->libelle_logement}}" desc="{{$logement->accroche_logement}}" note="{{$logement->moyenne_avis_logement}}" prix="{{$logement->prix_logement}}" lien="{{$logement->lien}}" id="{{$logement->id}}" natLogement="{{$logement->nature_logement}}" ville=""></x-Card>
                         <a class="btnHL {{$classBtnHL}}" href="{{route('setHL', ['id' => $logement->id])}}">{{$textbouton}}</a>
                         <a class="btnHL MODIF" href="{{route('updateLogement', ['id' => $logement->id])}}">Modifier</a>
                         <a class="btnHL SUPPR" href="{{route('delLogement', ['id' => $logement->id])}}">Supprimer</a>

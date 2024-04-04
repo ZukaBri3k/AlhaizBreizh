b47ee398dfe4ac73ec520c21a284747ab32c659b
RSymbiose <romain.jacovetti@etudiant.univ-rennes1.fr>
2024-04-04 18:09:13 +0200

    boutton retour avis

--- resources/views/components/reservation.blade.php
+++ resources/views/components/reservation.blade.php
@@ -7,5 +7,5 @@
     <p><strong>Du</strong> {{$dated}}</p>
     <p><strong>au</strong> {{$datef}}</p>
     <p><strong>{{$prix}} €</strong></p>
-    <button onclick="window.location.href='{{ route('creation_avis') }}'" id="btnTriPrix">Écrire un avis</button>
+    <button onclick="window.location.href='{{ route('retourAvis', ['id' => $id]) }}'" id="btnTriPrix">Écrire un avis</button>
 </div>

ce5f5ed9185d2049cd0ddb93e399cf62b492ce67
RSymbiose <romain.jacovetti@etudiant.univ-rennes1.fr>
2024-04-04 15:22:11 +0200

    recherche de proprio

--- app/Http/Controllers/Devis.php
+++ app/Http/Controllers/Devis.php
@@ -51,7 +51,7 @@ class Devis extends Controller
             $nombreDeJours = $nombreDeSecondes / (60 * 60 * 24);
     
             $prixtot = $nombreDeJours * $request->prix_tot;
-            $id_proprio = DB::select('select id_proprio_logement from logement where id_logement = ?', [$id_logement]);
+            $id_proprio = DB::select('select id_proprio_logement from logement where id_logement = ?', [intval($request->id)]);
             $proprietaire = DB::select('select nom_pers from personnes where id = ?', [intval($id_proprio[0]->id_proprio_logement)]);
 
             $tabDevis = [

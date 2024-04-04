24753abf9e844514cec7a7c6827e0146e55ae925
ZukaBri3k <kyrilldumerchat@gmail.com>
2024-04-04 17:45:23 +0200

    Fix URL issue in erreurModifCompte.js

--- public/js/erreurModifCompte.js
+++ public/js/erreurModifCompte.js
@@ -53,11 +53,12 @@ for (let i = 0; i < btnSubmit.length; i++) {
     btnSubmit[i].addEventListener('click', function (e) {
         e.preventDefault();
         url = this.url
+        console.log(url);
 
         let telephone = document.getElementsByClassName('Telephone');
 
         telephone[0].value = telephone[0].value.replace(/\s/g, '');
 
-        window.location.href = url;
+        //window.location.href = url;
     })
 }

c480dc4cfd7c9c0aa220da7e93f245fa6b381ab1
RSymbiose <romain.jacovetti@etudiant.univ-rennes1.fr>
2024-04-04 19:28:03 +0200

    test requete v6 la fin ? v6

--- app/View/Components/Reservation.php
+++ app/View/Components/Reservation.php
@@ -15,7 +15,6 @@ class Reservation extends Component
      */
 
     public $role;
-    public $avisExist;
 
     public function __construct(
         public string $libelle,
@@ -39,6 +38,12 @@ class Reservation extends Component
         $this->prix = $prix;
         $this->natlogement = $natlogement;
         $this->bool_resa = false;
+        if(Auth::check()) {
+            $this->id = Auth::user()->id;
+            $this->role = Auth::user()->role;
+        } else {
+            $this->role = null;
+        }
         $id_resa = DB::select('select id_reserv from reservation inner join devis on reservation.facture_reserv = devis.ref_devis inner join logement on reservation.id_logement_reserv = logement.id_logement where id_client_devis = ? and id_logement = ?', [auth()->user()->id, intval($this->idlogement)]);
                 if($id_resa != null){
                     $resa = DB::select('select id_reserv_avis from avis inner join reservation on avis.id_reserv_avis = reservation.id_reserv inner join logement on avis.id_logement_avis = logement.id_logement where id_reserv = ? and id_logement_avis = ?', [$id_resa[0]->id_reserv, intval($this->idlogement)]);

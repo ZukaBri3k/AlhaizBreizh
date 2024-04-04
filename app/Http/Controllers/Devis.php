783e2eca5085a31a156b154fde10dde9f2455488
RSymbiose <romain.jacovetti@etudiant.univ-rennes1.fr>
2024-04-04 12:25:54 +0200

    test pour la bdd v3

--- app/Http/Controllers/Devis.php
+++ app/Http/Controllers/Devis.php
@@ -52,7 +52,7 @@ class Devis extends Controller
     
             $prixtot = $nombreDeJours * $request->prix_tot;
             $id_proprio = DB::select('select id_proprio_logement from logement where id_logement = ?', [$id_logement]);
-            $proprietaire = DB::select('select nom_pers from personnes where id = ?', [$id_proprio[0]->id_proprio_logement]);
+            $proprietaire = DB::select('select nom_pers from personnes where id = ?', [intval($id_proprio[0]->id_proprio_logement)]);
 
             $tabDevis = [
                 NULL,

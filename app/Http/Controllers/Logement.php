1cedf8110b7c3865da785b5300b8b0ab1b3f9ae3
EwanP56 <ewanpenven@gmail.com>
2024-04-04 20:11:12 +0200

    back avis

--- app/Http/Controllers/Logement.php
+++ app/Http/Controllers/Logement.php
@@ -114,28 +114,23 @@ if ($files) {
     }
 
     public function getInfoLogement(Request $request) {
+        $bool_resa = false;
         if(!auth()->check()) {
             $id_role = 0;
-            $bool_resa = false;
         } else {
             if(auth()->user()->role == 1) {
                 $id_role = 1;
                 $id_resa = DB::select('select id_reserv from reservation inner join devis on reservation.facture_reserv = devis.ref_devis inner join logement on reservation.id_logement_reserv = logement.id_logement where id_client_devis = ? and id_logement = ?', [auth()->user()->id, intval($request->id)]);
                 if($id_resa != null){
                     foreach($id_resa as $values) {
-                        $resa = DB::select('select id_reserv_avis from avis inner join reservation on avis.id_reserv_avis = reservation.id_reserv inner join logement on avis.id_logement_avis = logement.id_logement where id_reserv = ? and id_logement_avis = ?', [$values, intval($request->id)]);
+                        $resa = DB::select('select id_reserv_avis from avis inner join reservation on avis.id_reserv_avis = reservation.id_reserv inner join logement on avis.id_logement_avis = logement.id_logement where id_reserv = ? and id_logement_avis = ?', [$values->id_reserv, intval($request->id)]);
                         if($resa != null) {
                             $bool_resa = true;
-                        } else {
-                            $bool_resa = false;
                         }
                     }
-                } else {
-                    $bool_resa = false;
                 }
             } else {
                 $id_role = 2;
-                $bool_resa = false;
             }
         }
         $id_proprio = DB::select('select id_proprio_logement from logement where id_logement = ?', [intval($request->id)]);

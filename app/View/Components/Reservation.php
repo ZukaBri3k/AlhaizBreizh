b7edd108de9b7a0d1150f269b553041c0090a07a
RSymbiose <romain.jacovetti@etudiant.univ-rennes1.fr>
2024-04-04 19:19:50 +0200

    test requete v6 la fin ? v4

--- app/View/Components/Reservation.php
+++ app/View/Components/Reservation.php
@@ -38,18 +38,18 @@ class Reservation extends Component
         $this->idreservation = $idreservation;
         $this->prix = $prix;
         $this->natlogement = $natlogement;
-        $bool_resa = false;
+        $this->bool_resa = false;
         $id_resa = DB::select('select id_reserv from reservation inner join devis on reservation.facture_reserv = devis.ref_devis inner join logement on reservation.id_logement_reserv = logement.id_logement where id_client_devis = ? and id_logement = ?', [auth()->user()->id, intval($this->idlogement)]);
                 if($id_resa != null){
                     $resa = DB::select('select id_reserv_avis from avis inner join reservation on avis.id_reserv_avis = reservation.id_reserv inner join logement on avis.id_logement_avis = logement.id_logement where id_reserv = ? and id_logement_avis = ?', [$id_resa[0]->id_reserv, intval($this->idlogement)]);
                     if($resa != null) {
-                        $bool_resa = true;
+                        $this->bool_resa = true;
                     }
                     else {
-                        $bool_resa = false;
+                        $this->bool_resa = false;
                     }
                 } else {
-                    $bool_resa = false;
+                    $this->bool_resa = false;
                 }
     }
 
@@ -59,7 +59,7 @@ class Reservation extends Component
     public function render(): View|Closure|string
     {
         return view('components.reservation', [
-            'bool_resa' => $bool_resa
+            'bool_resa' => $this->bool_resa,
         ]);
     }
 }

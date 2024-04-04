c67da4e0779f3aa79e1d4a30b4fe483e261d6b2d
RSymbiose <romain.jacovetti@etudiant.univ-rennes1.fr>
2024-04-04 18:04:43 +0200

    proprio v556

--- resources/views/components/reservation.blade.php
+++ resources/views/components/reservation.blade.php
@@ -7,5 +7,5 @@
     <p><strong>Du</strong> {{$dated}}</p>
     <p><strong>au</strong> {{$datef}}</p>
     <p><strong>{{$prix}} €</strong></p>
-    <button id="btnTriPrix" onclick="{{ route('creation_avis') }}">Écrire un avis</button>
+    <button onclick="window.location.href='{{ route('creation_avis') }}'" class="btnTriPrix">Écrire un avis</button>
 </div>

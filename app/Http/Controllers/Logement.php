190680f270d3284d19a7cde3fc475ea7b404bd8b
EwanP56 <ewanpenven@gmail.com>
2024-04-04 20:07:53 +0200

    back avis

--- app/Http/Controllers/Logement.php
+++ app/Http/Controllers/Logement.php
@@ -122,7 +122,7 @@ if ($files) {
                 $id_role = 1;
                 $id_resa = DB::select('select id_reserv from reservation inner join devis on reservation.facture_reserv = devis.ref_devis inner join logement on reservation.id_logement_reserv = logement.id_logement where id_client_devis = ? and id_logement = ?', [auth()->user()->id, intval($request->id)]);
                 if($id_resa != null){
-                    foreach($values as $id_resa) {
+                    foreach($id_resa as $values) {
                         $resa = DB::select('select id_reserv_avis from avis inner join reservation on avis.id_reserv_avis = reservation.id_reserv inner join logement on avis.id_logement_avis = logement.id_logement where id_reserv = ? and id_logement_avis = ?', [$values, intval($request->id)]);
                         if($resa != null) {
                             $bool_resa = true;

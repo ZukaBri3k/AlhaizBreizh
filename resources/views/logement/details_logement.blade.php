49276b1e100a7c37da157ad7804198f6a79d225f
EwanP56 <ewanpenven@gmail.com>
2024-04-04 15:09:38 +0200

    front avis

--- resources/views/logement/details_logement.blade.php
+++ resources/views/logement/details_logement.blade.php
@@ -420,7 +420,7 @@
               <img id="image_pp_previsu" src="{{ asset('pp/pp' . $values->id . '/img1.png')}}" class="pp" alt="photo de profil">
           @endif
             <div>
-              <p>{!! $values->pseudo_pers !!}</p>
+              <p class="note">{!! $values->pseudo_pers !!}</p>
               <p>{!! $values->ville_pers !!}, {!! $values->pays_pers !!}</p>
             </div>
           </div>
